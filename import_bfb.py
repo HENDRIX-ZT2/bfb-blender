10305790591ca3ac5ba4c770b006d7c56fccd61b
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-04-29 22:08:15 +0200

    reverse order of lods

--- import_bfb.py
+++ import_bfb.py
@@ -77,7 +77,7 @@ def import_scene_graph(b_parent, node, lod_level):
 			ob.parent_bone = "Bip01"
 			log_error(f"Capsule collider {node.name} has no parent bone, set to Bip01!")
 	# if we have children, the newly created empty is their parent
-	for child in node.children:
+	for child in reversed(node.children):
 		import_scene_graph(ob, child, lod_level)
 		# if this is a lod level, move next child to its respective layer
 		if ob.name.startswith("lodgroup"):
