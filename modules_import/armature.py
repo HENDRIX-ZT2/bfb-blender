206f28614d5c65f4c61119c0282c67649664144c
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-08-04 16:24:49 +0200

    improve fix_bone_lengths

--- modules_import/armature.py
+++ modules_import/armature.py
@@ -5,6 +5,9 @@ from common_bfb import create_ob, name_import
 from util.transforms import correction_local, correction_global
 
 
+TOLERANCE = 0.001
+
+
 def import_bones(basename, data, scales):
 	# create the b_armature_ob
 	b_armature_data = bpy.data.armatures.new(basename[:-4])
@@ -36,9 +39,8 @@ def import_bones(basename, data, scales):
 		b_edit_bone.head = bind.to_translation()
 		b_edit_bone.tail = tail + b_edit_bone.head
 		b_edit_bone.roll = roll
-	# fix the bone length
-	for edit_bone in b_armature_data.edit_bones:
-		fix_bone_length(edit_bone)
+	# fix the bone lengths
+	fix_bone_lengths(b_armature_data)
 	bpy.ops.object.mode_set(mode='OBJECT')
 	# priority
 	for bfb_bone in data.bones:
@@ -53,20 +55,40 @@ def get_matrix(matrix):
 	return matrix
 
 
-def fix_bone_length(edit_bone):
-	# don't change Bip01
-	if edit_bone.parent:
-		if edit_bone.children:
-			childheads = mathutils.Vector()
-			for child in edit_bone.children:
-				childheads += child.head
-			bone_length = (edit_bone.head - childheads / len(edit_bone.children)).length
-			if bone_length < 0.01:
-				bone_length = 0.25
-		# end of a chain
+def fix_bone_lengths(b_armature_data):
+	"""Sets all edit_bones to a suitable length."""
+	for b_edit_bone in b_armature_data.edit_bones:
+		# don't change root bones
+		if b_edit_bone.parent:
+			# calculate length based on position of children
+			if b_edit_bone.children:
+				children = [(len(b_child.children), b_child) for b_child in b_edit_bone.children]
+				# can't sort bones, so just sort counts
+				children.sort(key=lambda tup: tup[0], reverse=True)
+				# check if there is a bone that has more (recursive) children than the others
+				# if so, use that one to get the length
+				if len(children) > 1 and children[0][0] > children[1][0]:
+					bone_length = (b_edit_bone.head - children[0][1].head).length
+				else:
+					# average position of all children's heads
+					child_heads = mathutils.Vector()
+					for b_child in b_edit_bone.children:
+						child_heads += b_child.head
+					bone_length = (b_edit_bone.head - child_heads / len(b_edit_bone.children)).length
+			# end of a chain
+			else:
+				if (b_edit_bone.parent.tail - b_edit_bone.head).length < TOLERANCE:
+					# continues a chain from the parent
+					bone_length = b_edit_bone.parent.length
+				else:
+					# it is isolated from the parent, so make the bone smaller to uncluster the rig
+					bone_length = b_edit_bone.parent.length * 0.3
 		else:
-			bone_length = edit_bone.parent.length
-		edit_bone.length = bone_length
+			bone_length = b_edit_bone.length
+		# clamp to a safe minimum length
+		if bone_length < TOLERANCE:
+			bone_length = 0.1
+		b_edit_bone.length = bone_length
 
 
 def apply_rest_scale_correction(b_armature_ob, scales, anim, skinned_meshes):
