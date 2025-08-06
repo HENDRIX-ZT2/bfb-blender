c24d8763fdc13f98b3ebe621c95cde72b7e579de
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-08-06 18:49:26 +0200

    lower bf cleaning error default

--- modules_export/operators.py
+++ modules_export/operators.py
@@ -81,9 +81,9 @@ class ExportBF(ExportOp):
 	fix_tangents: BoolProperty(name="Fix Tangents",
 							   description="Smoothes tangents between actions",
 							   default=True)
-	error: FloatProperty(name="Max Cleaning Error",
+	error: FloatProperty(name="Cleaning Error",
 						 description="Adaptive Error - the more children a bone has, the less error it gets. The larger the error value, the smaller the file size, but the more error you get.",
-						 precision=3, step=1, soft_min=0.0, min=0.0, default=0.005)
+						 precision=3, step=0.1, soft_min=0.0, min=0.0, default=0.002)
 	exp_power: FloatProperty(name="Error Exponent",
 							 description="This influences how fast the error increases along the bone chain. Use larger values for a steeper falloff",
 							 precision=3, step=1, soft_min=1.0, min=1.0, default=1.0)
