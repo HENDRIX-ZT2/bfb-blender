35fe126d7902600abd363599df373b47302b56b4
HENDRIX-ZT2 <28632661+HENDRIX-ZT2@users.noreply.github.com>
2025-09-01 11:39:52 +0200

    add lin_to_srgb

--- util/colors.py
+++ util/colors.py
@@ -1,4 +1,9 @@
-def srgb_to_lin(img32):
-	mask = img32 >= 0.04045
-	img32[mask] = ((img32[mask] + 0.055) / 1.055)**2.4
-	img32[~mask] = img32[~mask] / 12.92
+def srgb_to_lin(img):
+	mask = img >= 0.04045
+	img[mask] = ((img[mask] + 0.055) / 1.055) ** 2.4
+	img[~mask] = img[~mask] / 12.92
+
+def lin_to_srgb(img):
+	mask = img > 0.0031308
+	img[mask] = 1.055 * (img ** (1.0 / 2.4)) - 0.055
+	img[~mask] = 12.92 * img
