eb74f2b88a0679e682b0c3b14d7e398e93729b05
HENDRIX-ZT2 <HENDRIX-ZT2@users.noreply.github.com>
2026-05-10 16:26:20 +0200

    bfb - fix lod generation

--- batch_bfb.py
+++ batch_bfb.py
@@ -1,3 +1,5 @@
+import logging
+
 import bpy
 import os
 import mathutils
@@ -6,92 +8,76 @@ import time
 from common_bfb import *
 from import_bfb import assign_to_lod
 
+
 def clear_scene():
 	#set the visible layers for this scene
 	# todo: make all collisions visible?
 	# bpy.context.scene.layers = [True for i in range(0,20)]
 	bpy.ops.object.select_all(action='SELECT')
 	bpy.ops.object.delete(use_global=True)
-	for cat in (bpy.data.objects, bpy.data.materials, ):
+	for cat in (bpy.data.objects, bpy.data.materials,):
 		for thing in cat:
 			thing.user_clear()
 			cat.remove(thing)
-	
-def get_children(childlist):
-	global oblist
-	for child in childlist:
-		oblist.append(child)
-		get_children(child.children)
 
 
 def add_lods(num_lods, rate):
-	ensure_active_object()
 	root = None
 	meshes = []
 	lodgroup = None
 	for ob in bpy.context.scene.objects:
-		ob.select_set(False)
 		if ob.type in ("EMPTY", "ARMATURE"):
 			if ob.name.startswith("lodgroup"):
-				print("BFB model already has lodgroups!")
-				ob.select_set(True)
+				logging.info("BFB model already has lodgroups!")
 				lodgroup = ob
 			if not ob.parent:
 				root = ob
 	# lodgroup mustn't be the root!
 	if not root:
-		root = create_empty(None,"AutoRoot",mathutils.Matrix())
+		root = create_empty(None, "AutoRoot", mathutils.Matrix())
 	# delete the existing lodgroup and low detail lods
 	for coll_name in bpy.context.view_layer.layer_collection.children.keys():
 		hide_collection(coll_name, False)
-	bpy.ops.object.select_all(action='DESELECT')
-	if lodgroup:
-		print("Found lodgroup, deleting children...")
-		try:
-			# parent lod0 to lodgroup's parent
-			lod0 = lodgroup.children[0]
-			lod0.parent = lodgroup.parent
-			global oblist
-			oblist = []
-			get_children(lodgroup.children)
-			for ob in oblist:
-				print("deleting",ob)
-				# first show, then select!
-				ob.hide_set(False, view_layer=bpy.context.view_layer)
-				ob.select_set(True)
-		except:
-			print("Deleted lonely lodgroup with no LOD levels!")
-		bpy.ops.object.delete(use_global=True)
+	if lodgroup and lodgroup.children:
+		logging.debug("Found lodgroup, deleting children...")
+		# parent lod0 to lodgroup's parent
+		lod0 = lodgroup.children[0]
+		lod0.parent = lodgroup.parent
+		# remove rest of the lodgroup and its children - add them later if needed
+		for ob in lodgroup.children_recursive:
+			bpy.data.objects.remove(ob)
+		bpy.data.objects.remove(lodgroup)
 	for ob in bpy.context.scene.objects:
 		# if we have more than one mesh we have to add a lod group node
-		# in some cases, a model is our root (eg. fence), then we add the lodgroup as the new root
+		# in some cases, a model is our root (e.g. fence), then we add the lodgroup as the new root
 		if ob.type == "MESH":
-			if ob.name.startswith('sphere') or ob.name.startswith('orientedbox') or ob.name.startswith('capsule'): pass
+			if ob.name.startswith('sphere') or ob.name.startswith('orientedbox') or ob.name.startswith('capsule'):
+				pass
 			else:
 				# special cases
 				if ob.parent_type == 'BONE':
 					# sometimes the main mesh is child of a bone...
 					ob.matrix_local = mathutils.Matrix()
-					# it has to be some NIF dummy object, so delete it!
 					if not ob.vertex_groups:
-						ob.select_set(True)
+						logging.warning(
+							f"{ob.name} was deleted as it was parented to a bone but had no vertex groups - NIF dummy object?")
+						bpy.data.objects.remove(ob)
 					# it shouldn't be parented to a bone, nif sometimes does this
 					else:
 						meshes.append(ob)
 				# this has to be lodded
 				else:
 					meshes.append(ob)
-	bpy.ops.object.delete(use_global=True)
-	
+
 	# only add a lodgroup if needed
 	if num_lods > 1:
-		lodgroup = create_empty(root,"lodgroup",mathutils.Matrix())
+		lodgroup = create_empty(root, "lodgroup", mathutils.Matrix())
 		# when obs are parented to empties with offset it will cause trouble!
 		# decide what the parent should be
 		for i in range(num_lods):
 			# create lod level if needed
 			if len(meshes) > 1:
-				lodlevel = create_empty(lodgroup,"LOD"+str(i),mathutils.Matrix())
+				lodlevel = create_empty(lodgroup, f"LOD{i}", mathutils.Matrix())
 				parent = lodlevel
 			else:
 				parent = lodgroup
@@ -101,18 +87,20 @@ def add_lods(num_lods, rate):
 				if i > 0:
 					lod = ob.copy()
 					lod.data = ob.data.copy()
-					lod.name = ob.name + "_LOD"+str(i)
-					bpy.context.scene.collection.objects.link(lod)
+					lod.name = f"{ob.name}_LOD{i}"
+					for coll in ob.users_collection:
+						coll.objects.link(lod)
 					assign_to_lod(lod, i)
 					lod.parent = parent
 					mod = lod.modifiers.new('Decimator', 'DECIMATE')
-					mod.ratio = 1/(i+rate)
-				else: 
+					mod.ratio = 1 / (i + rate)
+				else:
 					ob.parent = parent
 	# maybe a final cleanup
 	# if armature > clear any empties without children
-				
-def process(operator, context, files = [], filepath = "", num_lods = 1, rate = 1):
+
+
+def process(operator, context, files=[], filepath="", num_lods=1, rate=1):
 	dir = os.path.dirname(filepath)
 	starttime = time.time()
 
@@ -121,17 +109,22 @@ def process(operator, context, files = [], filepath = "", num_lods = 1, rate = 1
 
 	for file in files:
 		if file.name.endswith(".bfb"):
-			bpy.ops.import_scene.bluefang_bfb(filepath = os.path.join(dir, file.name), use_custom_normals = True)
+			bpy.ops.import_scene.bluefang_bfb(filepath=os.path.join(dir, file.name), use_custom_normals=True)
 			add_lods(num_lods, rate)
-			bpy.ops.export_scene.bluefang_bfb(filepath = os.path.join(dir, file.name)+"new.bfb", author_name="HENDRIX", export_materials = False)
+			bpy.ops.export_scene.bluefang_bfb(filepath=os.path.join(dir, file.name) + "new.bfb", author_name="HENDRIX",
+											  export_materials=False)
 		elif file.name.endswith(".nif"):
 			try:
-				bpy.ops.import_scene.nif(filepath = os.path.join(dir, file.name), combine_vertices = True, axis_forward='X', axis_up='Y')
+				bpy.ops.import_scene.nif(filepath=os.path.join(dir, file.name), combine_vertices=True, axis_forward='X',
+										 axis_up='Y')
 				add_lods(num_lods, rate)
-				bpy.ops.export_scene.bluefang_bfb(filepath = os.path.join(dir, file.name).replace(".nif",".bfb"), author_name="HENDRIX", export_materials = True)
-			except: print("NIF import didn't work")
-		else: continue
+				bpy.ops.export_scene.bluefang_bfb(filepath=os.path.join(dir, file.name).replace(".nif", ".bfb"),
+												  author_name="HENDRIX", export_materials=True)
+			except:
+				print("NIF import didn't work")
+		else:
+			continue
 		clear_scene()
-	success = '\nFinished Batch LOD processing in %.2f seconds\n' %(time.time()-starttime)
+	success = '\nFinished Batch LOD processing in %.2f seconds\n' % (time.time() - starttime)
 	print(success)
 	return {'FINISHED'}
